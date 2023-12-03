19cb876fb87bdcd556a3978ebe81f0ae20ddfaff
Tanmay <tanmaygautam1797@gmail.com>
2023-12-03 23:49:36 +0530

    first commit

---
+++ src/Nav.js
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from "react";
+import "./Nav.css";
+
+function Nav() {
+  const [show, handleshow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleshow(true);
+      } else handleshow(false);
+    });
+    return () => {
+      window.removeEventListener("scroll", null);
+    };
+  }, []);
+
+  return (
+    <div className={`nav ${show && "nav__black"}`}>
+      <img
+        className="nav__logo"
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1920px-Netflix_2015_logo.svg.png"
+        alt="Netflix Logo"
+      />
+      <img
+        className="nav__avatar"
+        src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+        alt="Netflix Avatar"
+      />
+    </div>
+  );
+}
+
+export default Nav;
